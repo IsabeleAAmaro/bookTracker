d5ea86c2c24a8db1117f017508bef2000278810a
isabele <isabeleamaro@edu.unirio.br>
2024-05-27 11:10:50 -0300

    Initial commit

---
+++ bookTracker/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bookTracker project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookTracker.settings')
+
+application = get_wsgi_application()
