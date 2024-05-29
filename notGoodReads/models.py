1ff76ebd9f7d975c078a89b31dcfa3842ea7527e
isabele <isabeleamaro@edu.unirio.br>
2024-05-29 11:10:01 -0300

    Adicionado models

--- notGoodReads/models.py
+++ notGoodReads/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+# Create your models here.1
+
+class Genero(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+class Usuario(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    senha = models.CharField(max_length=15)
+
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=100)
+    autor = models.CharField(max_length=100)
+    status_leitura = models.CharField(max_length=15)
+    genero = models.ForeignKey(Genero, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
