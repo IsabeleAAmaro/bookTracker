e773ed0da90ad971bebba4a9a9a85076eb952701
isabele <isabeleamaro@edu.unirio.br>
2024-06-15 21:48:47 -0300

    Adicionando forms e views

--- notGoodReads/views.py
+++ notGoodReads/views.py
@@ -3,12 +3,33 @@ from .models import Livro
 from models import Usuario
 from models import Genero
 
+
 # Create your views here.
-def livro_list(request):
+def livro_form(request):
     context = {'livro_list': Livro.objects.all()}
     return render(request, "notGoodReads/book_registration.html", context)
 
 
-def usuario_list(request):
+def livro_list(request):
+    context = {'livro_list': Livro.objects.all()}
+    return render(request, "notGoodReads/book_list.html", context)
+
+
+def livro_delete(request):
+    context = {'livro_list': Livro.objects.all()}
+    return render(request, "notGoodReads/user_configurations.html", context)
+
+
+def usuario_form(request):
     context = {'usuarios_list': Usuario.objects.all()}
     return render(request, "notGoodReads/user_signup.html", context)
+
+
+def usuario_list(request):
+    context = {'usuarios_list': Usuario.objects.all()}
+    return render(request, "notGoodReads/user_configurations.html", context)
+
+
+def usuario_delete(request):
+    context = {'usuarios_list': Usuario.objects.all()}
+    return render(request, "notGoodReads/user_configurations.html", context)
