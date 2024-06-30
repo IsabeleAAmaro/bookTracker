7a14e0a4030aa26ab12ae0a1241b0d925aa9af69
isabele <isabeleamaro@edu.unirio.br>
2024-06-30 15:30:25 -0300

    Adicionando forms e views

--- notGoodReads/forms.py
+++ notGoodReads/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Livro
+from .models import Usuario
 
 
 class LivroForm(forms.ModelForm):
@@ -23,3 +24,20 @@ class LivroForm(forms.ModelForm):
         self.fields['genero'].empty_label = "Select"
         self.fields['usuario'].empty_label = "Select"
         self.fields['isbn'].empty_label = "Select"
+
+
+class UsuarioForm(forms.ModelForm):
+    class Meta:
+        model = Usuario
+        fields = ('username', 'email', 'password')
+        labels = {
+            'username': 'Username',
+            'email': 'Email',
+            'password': '<PASSWORD>'
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(UsuarioForm, self).__init__(*args, **kwargs)
+        self.fields['username'].empty_label = "Select"
+        self.fields['email'].empty_label = "Select"
+        self.fields['password'].empty_label = "Select"
